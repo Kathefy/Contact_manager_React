02b95f771748a63a0e7f3888454d0b768febc147
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-01-30 21:05:05 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Header title="Contact Manager" />
+        <div className="container">
+          <Contact name="John Doe" email="fff@gmail.com" phone="555-55" />
+        </div>
+      </div>
+    );
   }
 }
-
 export default App;
