2b8c0f08e20a3e47e4f89c0fe84020246836e50d
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-02-14 21:52:37 +0100

    Updates

---
+++ precache-manifest.01120ae1d9431586cfa3694712bcb0c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d50c89470c166ba7fb21",
+    "url": "/contact_manager_react/static/js/runtime~main.d50c8947.js"
+  },
+  {
+    "revision": "eaeda571fb22e99aad7a",
+    "url": "/contact_manager_react/static/js/main.eaeda571.chunk.js"
+  },
+  {
+    "revision": "16f5a4f5f204117185da",
+    "url": "/contact_manager_react/static/js/1.16f5a4f5.chunk.js"
+  },
+  {
+    "revision": "eaeda571fb22e99aad7a",
+    "url": "/contact_manager_react/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "16f5a4f5f204117185da",
+    "url": "/contact_manager_react/static/css/1.540588a7.chunk.css"
+  },
+  {
+    "revision": "83f7284d538b26c0308c1fcaa24826c2",
+    "url": "/contact_manager_react/index.html"
+  }
+];
